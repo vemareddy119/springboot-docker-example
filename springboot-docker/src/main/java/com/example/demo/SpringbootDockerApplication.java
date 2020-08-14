b973a0f7bc61662d54599abfb21138393ac66791
vemareddy119 <vemareddy119@LAPTOP-2OSSR5PF>
2020-08-14 13:04:16 +0530

    First Commit.

---
+++ springboot-docker/src/main/java/com/example/demo/SpringbootDockerApplication.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDockerApplication {
+
+	@GetMapping("/")
+	public String getWelcome() {
+		return "Welcome to Spring boot docker";
+	}
+	
+	@GetMapping("/name")
+	public String getName() {
+		return "VEMAREDDY";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDockerApplication.class, args);
+	}
+
+}
